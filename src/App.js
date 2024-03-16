2c0c1b139f01c572b09fdb2a0fbd299388d501d4
danadi87 <dragulescumirela@gmail.com>
2024-03-16 09:51:47 +0100

    created the structure of the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to Baby's Kitchen!</h1>
+        </header>
+      </div>
+      <div className="Body">
+        <ul className="Body-ul">
+          <li>About</li>
+          <li>Shop</li>
+          <li>Contact</li>
+        </ul>
+      </div>
     </div>
   );
 }
